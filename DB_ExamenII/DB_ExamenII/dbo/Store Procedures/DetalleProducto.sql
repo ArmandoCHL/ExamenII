4afe7927138743398f83e30bc0fbf256f00bd70e
ArmandoCHL <90941239+ArmandoCHL@users.noreply.github.com>
2021-11-20 19:13:26 -0600

    Commt Both Projects

---
+++ DB_ExamenII/DB_ExamenII/dbo/Store Procedures/DetalleProducto.sql
@@ -0,0 +1,19 @@
+﻿CREATE PROCEDURE [dbo].[DetalleProducto]
+
+	@IdProducto int = NULL
+
+AS BEGIN
+	SET NOCOUNT ON
+
+
+	SELECT
+	
+		P.IdProducto,
+		P.NombreProducto,
+		P.PrecioProducto
+
+	FROM dbo.Producto P
+
+	WHERE (@IdProducto IS NULL OR IdProducto = @IdProducto)
+
+END
---
+++ DB_ExamenII/DB_ExamenII/dbo/Store Procedures/ListarOrdenes.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[ListarOrdenes]
+	
+
+AS BEGIN
+	SET NOCOUNT ON
+
+	SELECT * FROM dbo.Orden
+
+END
